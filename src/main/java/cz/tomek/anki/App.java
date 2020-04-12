f50f0cbe4f57d1b2dc50748bf7d606fc76547280
tomkre <krejcar.tomas@seznam.cz>
2020-04-12 14:26:29 +0200

    Code

---
+++ src/main/java/cz/tomek/anki/App.java
@@ -0,0 +1,9 @@
+package cz.tomek.anki;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
---
+++ src/test/java/cz/tomek/anki/AppIT.java
@@ -0,0 +1,15 @@
+package cz.tomek.anki;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class AppIT {
+	
+	@Test
+	public void shouldTest() {
+		String value = "value";
+		assertNotNull(value);
+	}
+
+}
