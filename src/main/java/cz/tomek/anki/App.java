f27af56002064b7edd9c92bbd983f7aede11bc96
tomkre <krejcar.tomas@seznam.cz>
2020-10-06 19:01:40 +0200

    playing

--- src/main/java/cz/tomek/anki/App.java
+++ src/main/java/cz/tomek/anki/App.java
@@ -1,9 +1,25 @@
 package cz.tomek.anki;
 
-public class App {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/app")
+public class App extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("<h1>Hello in Servlet!</h1>");
 	}
 	
 }
